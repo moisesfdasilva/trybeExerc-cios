3909b47bfca7ce1998e3d5df3aa14247bc9bdd3e
moisesfdasilva <msesfernandes@gmail.com>
2022-06-23 21:14:22 -0300

    Conclusão do exercício 2 do dia 4.1, code-02.js

---
+++ 01-Fundamentos/Bl-04-Introducao_a_JavaScript_e_Logica_de_Programacao/code-02.js
@@ -0,0 +1,18 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId)
+console.log(typeof isEnrolled)
+console.log(typeof patientInfo)
+
+//RETANGULO
+let largura = 5;
+let altura = 8;
+
+console.log("Área do retângulo 5 x 8: ", largura*altura)
+console.log("Perímetro do retângulo 5 x 8: ", (2*largura)+(2*altura))
