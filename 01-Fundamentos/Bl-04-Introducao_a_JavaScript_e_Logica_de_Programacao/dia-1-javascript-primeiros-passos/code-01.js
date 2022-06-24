ca6537e46638a0341fac4b5bdc5c73f7b98ca298
moisesfdasilva <msesfernandes@gmail.com>
2022-06-23 21:03:02 -0300

    Conclusão do exercício 1 do dia 4.1, code-01.js

--- 01-Fundamentos/Bl-04-Introducao_a_JavaScript_e_Logica_de_Programacao/dia-1-javascript-primeiros-passos/code-01.js
+++ 01-Fundamentos/Bl-04-Introducao_a_JavaScript_e_Logica_de_Programacao/dia-1-javascript-primeiros-passos/code-01.js
@@ -1,5 +1,16 @@
-let myName = 'Silva';
-let birthCity = 'Rio de Janeiro';
-let birthYear = '1989';
+//Primeira Saída
+let myName = "Silva";
+let birthCity = "Rio de Janeiro";
+let birthYear = 1989;
 
-console.log(myName);
+console.log(myName, birthCity, birthYear);
+
+//Segunda Saída
+let birthYear2 = 2030;
+
+console.log(birthYear2);
+
+//Terceira Saída
+let birthCity2 = "Florianópolis";
+
+console.log(birthCity2);
