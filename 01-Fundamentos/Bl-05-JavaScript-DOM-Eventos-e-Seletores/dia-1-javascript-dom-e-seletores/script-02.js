46165542a9f96b8b7fc2129dd6f553457e325a44
moisesfdasilva <msesfernandes@gmail.com>
2022-07-01 15:31:06 -0300

    realizada a segunda atividade do dia 5.1, em index-02.html, style-02.css e script-02.js

---
+++ 01-Fundamentos/Bl-05-JavaScript-DOM-Eventos-e-Seletores/dia-1-javascript-dom-e-seletores/script-02.js
@@ -0,0 +1,11 @@
+document.getElementById("header-container").style.backgroundColor = "green";
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "pink";
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = "yellow";
+
+document.getElementsByTagName("h3")[0].style.backgroundColor = "purple";
+document.getElementsByTagName("h3")[1].style.backgroundColor = "purple";
+
+document.getElementsByTagName("h3")[2].style.backgroundColor = "black";
+document.getElementsByTagName("h3")[3].style.backgroundColor = "black";
+
+document.querySelector("#footer-container").style.backgroundColor = "blue";
