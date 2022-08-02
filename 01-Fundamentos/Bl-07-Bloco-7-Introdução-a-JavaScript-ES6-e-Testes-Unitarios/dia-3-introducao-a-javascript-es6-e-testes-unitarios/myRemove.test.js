b5b57bfa50222462f7de46327a2f340ba41f18e9
moisesfdasilva <msesfernandes@gmail.com>
2022-08-02 15:48:31 -0300

    iniciando o bloco 8.

--- 01-Fundamentos/Bl-07-Bloco-7-Introdução-a-JavaScript-ES6-e-Testes-Unitarios/dia-3-introducao-a-javascript-es6-e-testes-unitarios/myRemove.test.js
+++
@@ -1,9 +0,0 @@
-const myRemove = require('./myRemove');
-
-describe('2 - Testa a função `myRemove`, conforme a seguir:', () => {
-  it('2.1 - Verifica se o array remove o valor solicitado.', () => {
-    expect(myRemove([1, 2, 3, 4], 3)).toEqual([ 1, 2, 4 ]);
-    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
-    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
-  });
-});
