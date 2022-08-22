73e00102dea0180eb32109abf561dab1d3bbe94b
moisesfdasilva <msesfernandes@gmail.com>
2022-08-22 20:13:38 -0300

    Realizada segunda atividade do dia 11.1

--- 02-Front-End/Bl-11-Componentes-com-Estado-Eventos-e-Formularios-com-React/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ 02-Front-End/Bl-11-Componentes-com-Estado-Eventos-e-Formularios-com-React/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,21 +1,24 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  const handleChange = (event) => {
+class App extends React.Component {
+  handleChange = (event) => {
     const result = `Clicou no ${event.target.innerText}`;
     console.log(result);
   };
 
-  return (
-    <div className="App">
-      <br/><br/><br/><br/>
-      <br/><br/><br/><br/>
-      {/* <input type="button" value="BOTÃO A" onclick={ handleChange }/> */}
-      <button onClick={ handleChange }>BOTÃO A</button>
-      <button onClick={ handleChange }>BOTÃO B</button>
-      <button onClick={ handleChange }>BOTÃO C</button>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <br/><br/><br/><br/>
+        <br/><br/><br/><br/>
+        {/* <input type="button" value="BOTÃO A" onclick={ handleChange }/> */}
+        <button onClick={ this.handleChange }>BOTÃO A</button>
+        <button onClick={ this.handleChange }>BOTÃO B</button>
+        <button onClick={ this.handleChange }>BOTÃO C</button>
+      </div>
+    );
+  }
 }
 
 export default App;
