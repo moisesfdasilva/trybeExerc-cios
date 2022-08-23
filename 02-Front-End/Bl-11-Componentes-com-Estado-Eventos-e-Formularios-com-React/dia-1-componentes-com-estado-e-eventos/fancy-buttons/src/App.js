e742639c9c0c812e2c744d308a04351e720309b0
moisesfdasilva <msesfernandes@gmail.com>
2022-08-22 22:42:00 -0300

    Realizada terceira atividade do dia 11.1

--- 02-Front-End/Bl-11-Componentes-com-Estado-Eventos-e-Formularios-com-React/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ 02-Front-End/Bl-11-Componentes-com-Estado-Eventos-e-Formularios-com-React/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -2,20 +2,32 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    buttonA: 0,
+    buttonB: 0,
+    buttonC: 0,
+  };
+
   handleChange = (event) => {
-    const result = `Clicou no ${event.target.innerText}`;
-    console.log(result);
+    // const result = `Clicou no ${event.target.innerText}`;
+    // console.log(result);
+    const { target } = event;
+    const { name } = target;
+    this.setState((previousState, _props) => ({
+      [name]: previousState[name] + 1,
+    }));
   };
 
   render() {
+    const { buttonA, buttonB, buttonC } = this.state;
     return (
       <div className="App">
         <br/><br/><br/><br/>
         <br/><br/><br/><br/>
         {/* <input type="button" value="BOTÃO A" onclick={ handleChange }/> */}
-        <button onClick={ this.handleChange }>BOTÃO A</button>
-        <button onClick={ this.handleChange }>BOTÃO B</button>
-        <button onClick={ this.handleChange }>BOTÃO C</button>
+        <button name="buttonA" type="button" onClick={ this.handleChange }>{ buttonA }</button>
+        <button name="buttonB" type="button" onClick={ this.handleChange }>{ buttonB }</button>
+        <button name="buttonC" type="button" onClick={ this.handleChange }>{ buttonC }</button>
       </div>
     );
   }
